8e6646cc31c7b9075a4e27224ce13a788ac48af0
saffochoice <1choice@mail.ru>
2018-05-26 11:34:27 +0300

    Icons generation created

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,13 @@
 $(function() {
 
-	// Custom JS
-
+	// CBlocks Initialization
+	$('.cblock').each(function(){
+		var data = $(this).attr('class').split(' ')[1];
+		var wrapper = $(this).find('.cblock__items');
+		$('.header__navblocks div').each(function(){
+			if($(this).data('category')==data){
+				wrapper.append($(this));
+			}
+		});
+	});
 });
